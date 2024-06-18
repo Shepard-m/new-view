a9480d4ed71a1bdecb1842ab644dd8f3c9773881
iluvator <petrov34rus@yandex.ru>
2024-06-18 23:27:46 +0300

    Реализовал поиск товаров

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,7 +1,52 @@
 import { Link } from 'react-router-dom';
+import { camerasSelectors } from '../../store/slice/catalog/catalog-selectros';
 import { AppRoute } from '../../const';
+import React, { ChangeEvent, useEffect, MouseEvent, useRef, useState, } from 'react';
+import { useAppSelector } from '../../types/indexStore';
+import { TProduct } from '../../types/product';
 
 export default function Header() {
+  const cameras = useAppSelector(camerasSelectors);
+  const formInput = useRef<HTMLInputElement>(null);
+  const formListModal = useRef<HTMLLIElement>(null);
+
+  const [valueInput, setValueInput] = useState<string>('');
+  const [listCamerasSearch, setListCamerasSearch] = useState<TProduct[]>([]);
+  const [isListOpen, setIsListOpen] = useState<boolean>(false);
+
+  function onClearSearchCameras(evt: MouseEvent) {
+    if (formInput.current !== evt.target ?? formListModal.current !== evt.target) {
+      setTimeout(() => {
+        setIsListOpen(false);
+        setValueInput('');
+      }, 500);
+      console.log(evt.target, formInput.current, formListModal.current);
+    }
+  }
+
+  useEffect(() => {
+    if (cameras !== null && valueInput.length >= 3) {
+      setIsListOpen(true);
+      setListCamerasSearch([...cameras].filter((camera) => camera.name.toLowerCase().includes(valueInput.toLowerCase())));
+    } else if (valueInput.length < 3) {
+      setIsListOpen(false);
+    }
+    window.addEventListener('mousedown', onClearSearchCameras);
+
+    return () => {
+      window.removeEventListener('mousedown', onClearSearchCameras);
+    };
+  }, [valueInput, cameras]);
+
+  function onSelectCamera() {
+    setIsListOpen(false);
+    setValueInput('');
+  }
+
+  function onSearchCameras(evt: ChangeEvent<HTMLInputElement>) {
+    setValueInput(evt.target.value);
+  }
+
   return (
     <header className="header" id="header" data-testid={'header'}>
       <div className="container">
@@ -22,6 +67,35 @@ export default function Header() {
             </li>
           </ul>
         </nav>
+        <div className={`form-search ${isListOpen ? 'list-opened' : ''}`}>
+          <form>
+            <label>
+              <svg className="form-search__icon" width={16} height={16} aria-hidden="true">
+                <use xlinkHref="#icon-lens" />
+              </svg>
+              <input className="form-search__input" value={valueInput} type="text" autoComplete="off" placeholder="Поиск по сайту" ref={formInput} onChange={onSearchCameras} />
+            </label>
+            <ul className="form-search__select-list">
+              {listCamerasSearch?.map((camera) => <Link key={camera.id} tabIndex={-1} to={`${AppRoute.CAMERA}/${camera.id}`}><li ref={formListModal} className="form-search__select-item" onClick={onSelectCamera} tabIndex={0}>{camera.name}</li></Link>)}
+            </ul>
+          </form>
+          <button className="form-search__reset" type="reset" onClick={onClearSearchCameras}>
+            <svg width={10} height={10} aria-hidden="true">
+              <use xlinkHref="#icon-close" />
+            </svg>
+            <span className="visually-hidden">
+              Сбросить поиск
+            </span>
+          </button>
+        </div>
+        <a className="header__basket-link" href="#">
+          <svg width={16} height={16} aria-hidden="true">
+            <use xlinkHref="#icon-basket" />
+          </svg>
+          <span className="header__basket-count">
+            3
+          </span>
+        </a>
       </div>
     </header>
   );
--- src/pages/catalog-page/catalog-page.tsx
+++ src/pages/catalog-page/catalog-page.tsx
@@ -49,120 +49,119 @@ export default function CatalogPage() {
               <div className="container">
                 <h1 className="title title--h2">Каталог фото- и видеотехники</h1>
                 <div className="page-content__columns">
-                  <div className="catalog__aside"><img src="img/banner.png" />
-                    {/*<div class="catalog-filter">
-                  <form action="#">
-                    <h2 class="visually-hidden">Фильтр</h2>
-                    <fieldset class="catalog-filter__block">
-                      <legend class="title title&#45;&#45;h5">Цена, ₽</legend>
-                      <div class="catalog-filter__price-range">
-                        <div class="custom-input">
+                  <div className="catalog-filter">
+                    <form action="#">
+                      <h2 className="visually-hidden">Фильтр</h2>
+                      <fieldset className="catalog-filter__block">
+                        <legend className="title title--h5">Цена, ₽</legend>
+                        <div className="catalog-filter__price-range">
+                          <div className="custom-input">
+                            <label>
+                              <input type="number" name="price" placeholder="от" />
+                            </label>
+                          </div>
+                          <div className="custom-input">
+                            <label>
+                              <input type="number" name="priceUp" placeholder="до" />
+                            </label>
+                          </div>
+                        </div>
+                      </fieldset>
+                      <fieldset className="catalog-filter__block">
+                        <legend className="title title--h5">Категория</legend>
+                        <div className="custom-radio catalog-filter__item">
                           <label>
-                            <input type="number" name="price" placeholder="от">
+                            <input type="radio" name="category" defaultValue="photocamera" defaultChecked /><span className="custom-radio__icon" /><span className="custom-radio__label">Фотокамера</span>
                           </label>
                         </div>
-                        <div class="custom-input">
+                        <div className="custom-radio catalog-filter__item">
                           <label>
-                            <input type="number" name="priceUp" placeholder="до">
+                            <input type="radio" name="category" defaultValue="videocamera" /><span className="custom-radio__icon" /><span className="custom-radio__label">Видеокамера</span>
                           </label>
                         </div>
-                      </div>
-                    </fieldset>
-                    <fieldset class="catalog-filter__block">
-                      <legend class="title title&#45;&#45;h5">Категория</legend>
-                      <div class="custom-radio catalog-filter__item">
-                        <label>
-                          <input type="radio" name="category" value="photocamera" checked><span class="custom-radio__icon"></span><span class="custom-radio__label">Фотокамера</span>
-                        </label>
-                      </div>
-                      <div class="custom-radio catalog-filter__item">
-                        <label>
-                          <input type="radio" name="category" value="videocamera"><span class="custom-radio__icon"></span><span class="custom-radio__label">Видеокамера</span>
-                        </label>
-                      </div>
-                    </fieldset>
-                    <fieldset class="catalog-filter__block">
-                      <legend class="title title&#45;&#45;h5">Тип камеры</legend>
-                      <div class="custom-checkbox catalog-filter__item">
-                        <label>
-                          <input type="checkbox" name="digital" checked><span class="custom-checkbox__icon"></span><span class="custom-checkbox__label">Цифровая</span>
-                        </label>
-                      </div>
-                      <div class="custom-checkbox catalog-filter__item">
-                        <label>
-                          <input type="checkbox" name="film" disabled><span class="custom-checkbox__icon"></span><span class="custom-checkbox__label">Плёночная</span>
-                        </label>
-                      </div>
-                      <div class="custom-checkbox catalog-filter__item">
-                        <label>
-                          <input type="checkbox" name="snapshot"><span class="custom-checkbox__icon"></span><span class="custom-checkbox__label">Моментальная</span>
-                        </label>
-                      </div>
-                      <div class="custom-checkbox catalog-filter__item">
-                        <label>
-                          <input type="checkbox" name="collection" checked disabled><span class="custom-checkbox__icon"></span><span class="custom-checkbox__label">Коллекционная</span>
-                        </label>
-                      </div>
-                    </fieldset>
-                    <fieldset class="catalog-filter__block">
-                      <legend class="title title&#45;&#45;h5">Уровень</legend>
-                      <div class="custom-checkbox catalog-filter__item">
-                        <label>
-                          <input type="checkbox" name="zero" checked><span class="custom-checkbox__icon"></span><span class="custom-checkbox__label">Нулевой</span>
-                        </label>
-                      </div>
-                      <div class="custom-checkbox catalog-filter__item">
-                        <label>
-                          <input type="checkbox" name="non-professional"><span class="custom-checkbox__icon"></span><span class="custom-checkbox__label">Любительский</span>
-                        </label>
-                      </div>
-                      <div class="custom-checkbox catalog-filter__item">
-                        <label>
-                          <input type="checkbox" name="professional"><span class="custom-checkbox__icon"></span><span class="custom-checkbox__label">Профессиональный</span>
-                        </label>
-                      </div>
-                    </fieldset>
-                    <button class="btn catalog-filter__reset-btn" type="reset">Сбросить фильтры
-                    </button>
-                  </form>
-                </div>*/}
+                      </fieldset>
+                      <fieldset className="catalog-filter__block">
+                        <legend className="title title--h5">Тип камеры</legend>
+                        <div className="custom-checkbox catalog-filter__item">
+                          <label>
+                            <input type="checkbox" name="digital" defaultChecked /><span className="custom-checkbox__icon" /><span className="custom-checkbox__label">Цифровая</span>
+                          </label>
+                        </div>
+                        <div className="custom-checkbox catalog-filter__item">
+                          <label>
+                            <input type="checkbox" name="film" disabled /><span className="custom-checkbox__icon" /><span className="custom-checkbox__label">Плёночная</span>
+                          </label>
+                        </div>
+                        <div className="custom-checkbox catalog-filter__item">
+                          <label>
+                            <input type="checkbox" name="snapshot" /><span className="custom-checkbox__icon" /><span className="custom-checkbox__label">Моментальная</span>
+                          </label>
+                        </div>
+                        <div className="custom-checkbox catalog-filter__item">
+                          <label>
+                            <input type="checkbox" name="collection" defaultChecked disabled /><span className="custom-checkbox__icon" /><span className="custom-checkbox__label">Коллекционная</span>
+                          </label>
+                        </div>
+                      </fieldset>
+                      <fieldset className="catalog-filter__block">
+                        <legend className="title title--h5">Уровень</legend>
+                        <div className="custom-checkbox catalog-filter__item">
+                          <label>
+                            <input type="checkbox" name="zero" defaultChecked /><span className="custom-checkbox__icon" /><span className="custom-checkbox__label">Нулевой</span>
+                          </label>
+                        </div>
+                        <div className="custom-checkbox catalog-filter__item">
+                          <label>
+                            <input type="checkbox" name="non-professional" /><span className="custom-checkbox__icon" /><span className="custom-checkbox__label">Любительский</span>
+                          </label>
+                        </div>
+                        <div className="custom-checkbox catalog-filter__item">
+                          <label>
+                            <input type="checkbox" name="professional" /><span className="custom-checkbox__icon" /><span className="custom-checkbox__label">Профессиональный</span>
+                          </label>
+                        </div>
+                      </fieldset>
+                      <button className="btn catalog-filter__reset-btn" type="reset">Сбросить фильтры
+                      </button>
+                    </form>
                   </div>
                   <div className="catalog__content">
-                    {/*<div class="catalog-sort">
-                  <form action="#">
-                    <div class="catalog-sort__inner">
-                      <p class="title title&#45;&#45;h5">Сортировать:</p>
-                      <div class="catalog-sort__type">
-                        <div class="catalog-sort__btn-text">
-                          <input type="radio" id="sortPrice" name="sort" checked>
-                          <label for="sortPrice">по цене</label>
+                    <div className="catalog-sort">
+                      <form action="#">
+                        <div className="catalog-sort__inner">
+                          <p className="title title--h5">Сортировать:</p>
+                          <div className="catalog-sort__type">
+                            <div className="catalog-sort__btn-text">
+                              <input type="radio" id="sortPrice" name="sort" defaultChecked />
+                              <label htmlFor="sortPrice">по цене</label>
+                            </div>
+                            <div className="catalog-sort__btn-text">
+                              <input type="radio" id="sortPopular" name="sort" />
+                              <label htmlFor="sortPopular">по популярности</label>
+                            </div>
+                          </div>
+                          <div className="catalog-sort__order">
+                            <div className="catalog-sort__btn catalog-sort__btn--up">
+                              <input type="radio" id="up" name="sort-icon" defaultChecked aria-label="По возрастанию" />
+                              <label htmlFor="up">
+                                <svg width={16} height={14} aria-hidden="true">
+                                  <use xlinkHref="#icon-sort" />
+                                </svg>
+                              </label>
+                            </div>
+                            <div className="catalog-sort__btn catalog-sort__btn--down">
+                              <input type="radio" id="down" name="sort-icon" aria-label="По убыванию" />
+                              <label htmlFor="down">
+                                <svg width={16} height={14} aria-hidden="true">
+                                  <use xlinkHref="#icon-sort" />
+                                </svg>
+                              </label>
+                            </div>
+                          </div>
                         </div>
-                        <div class="catalog-sort__btn-text">
-                          <input type="radio" id="sortPopular" name="sort">
-                          <label for="sortPopular">по популярности</label>
-                        </div>
-                      </div>
-                      <div class="catalog-sort__order">
-                        <div class="catalog-sort__btn catalog-sort__btn&#45;&#45;up">
-                          <input type="radio" id="up" name="sort-icon" checked aria-label="По возрастанию">
-                          <label for="up">
-                            <svg width="16" height="14" aria-hidden="true">
-                              <use xlink:href="#icon-sort"></use>
-                            </svg>
-                          </label>
-                        </div>
-                        <div class="catalog-sort__btn catalog-sort__btn&#45;&#45;down">
-                          <input type="radio" id="down" name="sort-icon" aria-label="По убыванию">
-                          <label for="down">
-                            <svg width="16" height="14" aria-hidden="true">
-                              <use xlink:href="#icon-sort"></use>
-                            </svg>
-                          </label>
-                        </div>
-                      </div>
+                      </form>
                     </div>
-                  </form>
-                </div>*/}
+
                     <CatalogProducts cameras={cameras} />
                     {/*<div class="pagination">
                   <ul class="pagination__list">
