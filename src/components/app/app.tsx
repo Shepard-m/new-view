a32eb707b4702a861fb3f165a516b3a5aab8f403
iluvator <petrov34rus@yandex.ru>
2024-08-26 23:26:31 +0300

    fix

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -10,7 +10,7 @@ export default function App() {
     <Routes>
       <Route path={AppRoute.CATALOG}>
         <Route
-          path={'/nev-view'}
+          index
           element={<CatalogPage />}
         />
         <Route
--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'new-view/',
+  base: '/nev-view',
   plugins: [react()],
   test: {
     globals: true,
