71a9fcd62e4d8cf103381fcd4010a8bffed34597
iluvator <petrov34rus@yandex.ru>
2024-08-26 23:29:07 +0300

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/nev-view',
+  base: '/new-view',
   plugins: [react()],
   test: {
     globals: true,
